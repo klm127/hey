a69fa8bb7910f671fa781549c5bf13d273b4423d
klm127 <karlmiller127@gmail.com>
2021-11-26 21:49:57 -0500

    basic render 📷

--- tests/test-html.js
+++ tests/test-html.js
@@ -3,5 +3,6 @@ const hey = require('../lib/hey');
 
 
 let app = hey('app');
-
+app.add.child().named('top').with.child().named('left').with.sibling().named('right');
+app.add.child().named('middle').with.child().named('left');
 app.render('html');
